b9016c36c7a96e091040e2b48a0b7e840a5435c3
abdullaxows <abdulla.abdullazade.2008@gmail.com>
2025-04-15 11:47:58 +0400

    main

---
+++ backend.py
@@ -0,0 +1,73 @@
+from fastapi import FastAPI
+import edge_tts
+from uuid import uuid4
+import os
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi import Request
+from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+
+
+class TTSRequest(BaseModel):
+    text: str
+    voice: str
+
+
+class TTSDeleteRequest(BaseModel):
+    filename: str
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/audio", StaticFiles(directory="audio"), name="audio")
+
+templates = Jinja2Templates(directory="frontend")
+
+
+async def text_to_speech(text, voice):
+
+    try:
+        filename_base = f"audio/{uuid4()}"
+        communicate = edge_tts.Communicate(text, voice=voice)
+        await communicate.save(f"{filename_base}.ogg")
+        return f"{filename_base}.ogg"
+    except Exception as e:
+        print("TTS generation failed:", e)
+        return None
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("app.html", {"request": request})
+
+
+@app.post("/api/tts")
+async def audio_link(body: TTSRequest):
+    text = body.text
+    voice = body.voice
+    print("Received text:", text)
+    os.makedirs("audio", exist_ok=True)
+    audio_link = await text_to_speech(text, voice)
+    return {"audio_link": audio_link}
+
+
+@app.delete("/api/delete")
+async def delete_audio(body: TTSDeleteRequest):
+    filename = body.filename
+    print("Received filename:", filename)
+    try:
+        os.remove(filename)
+        return {"message": "File deleted "}
+    except Exception as e:
+        return {"error": str(e)}
